190622df0f1c8f75bb9ac8798856c0979c666b26
Jonathan E. Schmidt <jschmi17@student.aau.dk>
2023-04-20 14:45:49 +0200

    Added world files and launch

---
+++ launch/cohan_stage_pr2_comparison.launch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    this_directory = get_package_share_directory('stage_ros2')
+
+    stage_world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=TextSubstitution(text='comparison'),
+        description='World file relative to the project world file, without .world')
+
+    def stage_world_configuration(context):
+        file = os.path.join(
+            this_directory,
+            'world',
+            context.launch_configurations['world'] + '.world')
+        return [SetLaunchConfiguration('world_file', file)]
+
+    stage_world_configuration_arg = OpaqueFunction(
+        function=stage_world_configuration)
+
+    urdf = os.path.join(
+        this_directory,
+        'world/urdf/pr2.urdf')
+
+    return LaunchDescription([
+        stage_world_arg,
+        stage_world_configuration_arg,
+        Node(
+            package='stage_ros2',
+            executable='stage_ros2',
+            name='stage',
+            parameters=[{
+                "world_file": [LaunchConfiguration('world_file')]}],
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            arguments=[urdf]),
+        Node(
+            name='joint_state_publisher',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            parameters=[{'source_list': ["/stage_joint_states"]}],
+        ),
+        Node(
+            name='stage_joints',
+            package='stage_ros2_scripts',
+            executable='stage_joints'
+        )
+
+    ])
