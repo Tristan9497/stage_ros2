ab4d07453a31d89430acc95fdce73c376569e2f9
Tristan Schwörer <tschwo21@student.aau.dk>
2023-04-24 12:38:52 +0200

    push changes

--- launch/cohan_stage_pr2_comparison.launch.py
+++ launch/cohan_stage_pr2_comparison.launch.py
@@ -4,14 +4,30 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, TextSubstitution
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
+from launch.substitutions import LaunchConfiguration, TextSubstitution, PythonExpression
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration, ExecuteProcess, IncludeLaunchDescription, LogInfo
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
 
     this_directory = get_package_share_directory('stage_ros2')
+
+    bringup_dir = get_package_share_directory('context_aware_navigation')
+    launch_dir = os.path.join(bringup_dir, 'launch')
+
+    # Create the launch configuration variables
+    slam = LaunchConfiguration('slam')
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    map_yaml_file = LaunchConfiguration('map')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    params_file = LaunchConfiguration('params_file')
+    autostart = LaunchConfiguration('autostart')
+    use_composition = LaunchConfiguration('use_composition')
+    use_respawn = LaunchConfiguration('use_respawn')
 
     stage_world_arg = DeclareLaunchArgument(
         'world',
@@ -25,6 +41,49 @@ def generate_launch_description():
             context.launch_configurations['world'] + '.world')
         return [SetLaunchConfiguration('world_file', file)]
 
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='pr2',
+        description='Top-level namespace')
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='true',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_slam_cmd = DeclareLaunchArgument(
+        'slam',
+        default_value='False',
+        description='Whether run a SLAM')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(
+            this_directory, 'world', 'maps', 'comparison.yaml'),
+        description='Full path to map file to load')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        'use_composition', default_value='True',
+        description='Whether to use composed bringup')
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        'use_respawn', default_value='False',
+        description='Whether to respawn if a node crashes. Applied when composition is disabled.')
+
     stage_world_configuration_arg = OpaqueFunction(
         function=stage_world_configuration)
 
@@ -32,7 +91,21 @@ def generate_launch_description():
         this_directory,
         'world/urdf/pr2.urdf')
 
+    bringup_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'bringup_launch.py')),
+        launch_arguments={'namespace': namespace,
+                          'use_namespace': use_namespace,
+                          'slam': slam,
+                          'map': map_yaml_file,
+                          'use_sim_time': use_sim_time,
+                          'params_file': params_file,
+                          'autostart': autostart,
+                          'use_composition': use_composition,
+                          'use_respawn': use_respawn}.items())
+
     return LaunchDescription([
+        declare_map_yaml_cmd,
         stage_world_arg,
         stage_world_configuration_arg,
         Node(
@@ -47,18 +120,28 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': True,
+                         'frame_prefix': 'pr2/'}],
             arguments=[urdf]),
-        Node(
-            name='joint_state_publisher',
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            parameters=[{'source_list': ["/stage_joint_states"]}],
-        ),
-        Node(
-            name='stage_joints',
-            package='stage_ros2_scripts',
-            executable='stage_joints'
-        )
+        # Node(
+        #     name='joint_state_publisher',
+        #     package='joint_state_publisher',
+        #     executable='joint_state_publisher',
+        #     parameters=[{'source_list': ["/stage_joint_states"]}],
+        # ),
+        # Node(
+        #     name='stage_joints',
+        #     package='stage_ros2_scripts',
+        #     executable='stage_joints'
+        # ),
+        declare_namespace_cmd,
+        declare_use_namespace_cmd,
+        declare_slam_cmd,
+        declare_use_sim_time_cmd,
+        declare_params_file_cmd,
+        declare_autostart_cmd,
+        declare_use_composition_cmd,
+        declare_use_respawn_cmd,
+        bringup_cmd
 
     ])
